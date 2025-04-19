000c2cb42c6554f23f186fb02551e696dae6e6eb
harshalsakhare2305 <harshalsakhare2305@gmail.com>
2025-04-19 20:06:17 +0530

    Initial Commit

---
+++ makemyresume/types/generated/components.d.ts
@@ -0,0 +1,70 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'education';
+    icon: 'book';
+  };
+  attributes: {
+    degree: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+    endDate: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    universityName: Schema.Attribute.String;
+  };
+}
+
+export interface ExperienceExperience extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'Experience';
+    icon: 'alien';
+  };
+  attributes: {
+    city: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+    workSummery: Schema.Attribute.Text;
+  };
+}
+
+export interface ProjectsProjects extends Struct.ComponentSchema {
+  collectionName: 'components_projects_projects';
+  info: {
+    displayName: 'Projects';
+    icon: 'check';
+  };
+  attributes: {
+    githubLink: Schema.Attribute.String;
+    projectDescription: Schema.Attribute.Text;
+    projectName: Schema.Attribute.String;
+  };
+}
+
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'skills';
+    icon: 'brush';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'education.education': EducationEducation;
+      'experience.experience': ExperienceExperience;
+      'projects.projects': ProjectsProjects;
+      'skills.skills': SkillsSkills;
+    }
+  }
+}
